8ac159d022172b0115620501bc7966fdbf947e04
Clara-Musyoka <musyokamusenya@gmail.com>
2023-07-24 16:34:17 +0300

    Add DB connection

--- src/main/java/org/safaricom/DB.java
+++ src/main/java/org/safaricom/DB.java
@@ -1,4 +1,9 @@
 package org.safaricom;
 
+import org.sql2o.Sql2o;
+
 public class DB {
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/weektwo", "postgres", "clara");
 }
+
+
